b83d87623bec810f768570f6d6f61489c5524e65
michaelsatumba <mjsatumba@gmail.com>
2022-06-14 13:21:21 -0700

    comment out router

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import { useRouter } from 'next/router';
 import { useState } from 'react';
 
 export default function Home() {
-	const router = useRouter();
+	// const router = useRouter();
 
 	const [zipCode, setZipCode] = useState('93030');
 	const [state, setState] = useState(6);
--- pages/results.js
+++ pages/results.js
@@ -4,16 +4,16 @@ import { useState } from 'react';
 // import puppeteer from 'puppeteer';
 
 function results() {
-	const router = useRouter();
+	// const router = useRouter();
 
-	const {
-		query: { zipCode, state },
-	} = router;
+	// const {
+	// 	query: { zipCode, state },
+	// } = router;
 
-	const props = {
-		zipCode,
-		state,
-	};
+	// const props = {
+	// 	zipCode,
+	// 	state,
+	// };
 
 	const [link, setLink] = useState(
 		`https://www.irs.gov/efile-index-taxpayer-search?zip=${props.zipCode}&state=${props.state}`
